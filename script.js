8657c92f54fd95eae9f1db1cc75f527752692569
Areg Ayvazyan <ayvazyan.areg2015@mail.ru>
2023-02-22 21:56:04 +0400

    DATE

---
+++ script.js
@@ -0,0 +1,31 @@
+const date = new Date();
+
+const el1 = document.getElementById("1");
+
+const date1 = (`0${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
+
+el1.innerHTML = date1;
+
+//
+// 
+const el2 = document.getElementById("2");
+
+const date2 = (`0${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
+
+el2.innerHTML = date2;
+
+// 
+// 
+const el3 = document.getElementById("3");
+
+const date3 = (`0${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
+
+el3.innerHTML = date3;
+
+// 
+//
+const el4 = document.getElementById("4");
+
+const date4 = (`0${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
+
+el4.innerHTML = date4;
